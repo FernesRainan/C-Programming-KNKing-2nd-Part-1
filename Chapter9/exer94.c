cc8f76e554d776832164c0f04a7084928ccc3a25
FernesRainan <fernes.rainan@gmail.com>
2015-10-13 12:26:57 +0900

    Create exer94.c

---
+++ Chapter9/exer94.c
@@ -0,0 +1,13 @@
+int day_of_year (int month, int day, int year)
+{
+	int num_days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	int day_count = 0, i;
+	
+	for (i = 1; i < month; i++)
+		day_count += num_day[i - 1];
+	
+	if ((year % 4 == 0 && (year % 100 != 0 || year % 400 ==0)) && month > 2)
+		day_count++;
+		
+	return day_count + day;
+}
