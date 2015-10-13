b9db13d3d8a874536f89cd522e8236bba9ba21da
FernesRainan <fernes.rainan@gmail.com>
2015-10-13 22:53:54 +0900

    Create exer914.c

---
+++ Chapter9/exer914.c
@@ -0,0 +1,24 @@
+//How to fix it//
+
+bool has_zero (int a[], int n)
+{
+	int i;
+	
+	for (i = 0; i < n; i++)
+		if (a[i] == 0)
+			return true;
+		else 
+			return false; //Bug//
+}
+	
+
+bool has_zero (int a[], int n)
+{
+	int i
+	
+	for ( i = 0; i < n; i++)
+		if (a[i] == 0)
+			return true;
+	
+	return false;
+}
