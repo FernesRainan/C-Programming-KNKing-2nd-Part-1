062b21752484475bfd08ead5d20e0899d536057e
FernesRainan <fernes.rainan@gmail.com>
2015-10-13 22:39:54 +0900

    Create exer913.c

---
+++ Chapter9/exer913.c
@@ -0,0 +1,37 @@
+/* Write the following function, which evaluates a chess position:
+int evaluate_position (char board[8][8]);
+board represents a configuration of pieces on a chessboard, where the letters K, Q, R, B, N, P 
+represent White pieces, and the letters k, q, r, b, n and p represent Black pieces.
+evaluate_position shoud sum the values of the White pieces ( Q=9, R=5, B=3, N=3, P=1). 
+It should also sum the values of the Black pieces (done in a similar way). 
+The function will return the difference between the two numbers. This value will be positive if
+White has an advantage in material and negative if Black has an advantage */
+
+int evaluate_position (char board[8][8])
+{
+	char piece;
+	int white = 0, black = 0;
+	int i, j;
+	
+	for (i = 0; i < 8; i++){
+		for (j = 0; j < 8; j++){
+			piece = board[i][j];
+			
+			switch (piece) {
+				case 'Q': white += 9; break;
+				case 'R': white += 5; break;
+				case 'B': white += 3; break;
+				case 'N': white += 3; break;
+				case 'P': white += 1; break;
+					
+				case 'q': black += 9; break;
+				case 'r': black += 5; break;
+				case 'b': black += 3; break;
+				case 'n': black += 3; break;
+				case 'p': black += 1; break;
+			}
+		}
+	}
+	
+	return white - black;
+}
