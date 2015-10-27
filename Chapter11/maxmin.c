43b8fa26ee233837b2162a00ba6ce380ba90ffac
FernesRainan <fernes.rainan@gmail.com>
2015-10-27 22:06:55 +0900

    Update maxmin.c

--- Chapter11/maxmin.c
+++ Chapter11/maxmin.c
@@ -14,12 +14,12 @@ int main (void)
 	for (i = 0; i < N; i++)
 		scanf("%d", &b[i]);
 		
-		max_min(b, N, &big, &small);
+	max_min(b, N, &big, &small);
 		
-		printf("Largest: %d\n", big);
-		printf("Smallest: %d\n", small);
+	printf("Largest: %d\n", big);
+	printf("Smallest: %d\n", small);
 		
-		return 0;
+	return 0;
 }
 
 void max_min (int a[], int n, int *max, int *min)
