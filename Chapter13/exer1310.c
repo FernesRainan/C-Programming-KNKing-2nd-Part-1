275cfbd914ffab1e684d1226dc2f49b4372a867d
FernesRainan <fernes.rainan@gmail.com>
2015-11-06 17:06:18 +0900

    Create exer1310.c

---
+++ Chapter13/exer1310.c
@@ -0,0 +1,23 @@
+// What will be the value of the string s1 after the following statements have been executed?
+
+#include <stdio.h>
+#include <string.h>
+
+int main (void)
+{
+	char s1[20], s2[20];
+	
+	strcpy(s1, "computer");
+	strcpy(s2, "science");
+	
+	if (strcmp(s1, s2) < 0)
+		strcat (s1, s2);
+	else
+		strcat (s2, s1);
+	
+	s1[strlen(s1)- 6] = '\0';
+	
+	printf("Test :: %s\n", s1);
+	
+	return 0;
+}
