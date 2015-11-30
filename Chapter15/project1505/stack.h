dd1437c4a2b20824d407db01583656efab08e684
FernesRainan <fernes.rainan@gmail.com>
2015-11-30 23:06:16 +0900

    Create stack.h

---
+++ Chapter15/project1505/stack.h
@@ -0,0 +1,28 @@
+#ifndef STACK_H
+#define STACK_H
+
+void too_complex(void);
+
+void not_enough (void);
+
+void stack_underflow(void);
+
+void stack_overflow(void);
+
+void make_empty (void);
+
+bool is_empty (void);
+
+bool is_full(void);
+
+void push (int operand);
+
+char pop (void);
+
+void warn_incomplete (void);
+
+void subtract(void);
+
+void divide (void);
+
+#endif
