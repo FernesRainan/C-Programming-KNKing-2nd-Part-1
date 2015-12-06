4d93e2abc127ea6a035866ebe2b6d31cb8059c90
FernesRainan <fernes.rainan@gmail.com>
2015-12-06 14:58:19 +0900

    Create exer1602b.c

---
+++ Chapter16/exer1602b.c
@@ -0,0 +1,7 @@
+// Modify the declaration in part (a) so that c1's members initially have values 0.0
+// and 1.0, while c2's members are 1.0 and 0.0 initially. ( c3 is not initialized)
+
+struct {
+	double real, imaginary;
+} c1 = {0.0, 1.0}, c2 = {1.0, 0.0}, c3;
+
