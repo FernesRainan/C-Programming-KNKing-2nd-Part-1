aa57035a41bff49e0b7c9525c10650b48d1220f6
FernesRainan <fernes.rainan@gmail.com>
2015-12-16 11:20:20 +0900

    Create exer1615_enum.c

---
+++ Chapter16/exer1615_enum.c
@@ -0,0 +1,11 @@
+// (a) Declare a tag for an enumeration whose values represent the seven days of the week.
+
+enum week_days {SUN, MON, TUE, WED, THU, FRI, SAT};
+
+// (b) Use typedef to define a name for the enumeration of part(a)
+
+typedef enum {SUN, MON, TUE, WED, THU, FRI, SAT} Week_days;
+
+
+
+ 
