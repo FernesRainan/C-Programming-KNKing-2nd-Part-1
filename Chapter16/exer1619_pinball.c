3f5f710af828b7daf8ffcb33538b7bfa7fe8adfb
FernesRainan <fernes.rainan@gmail.com>
2015-12-18 22:03:29 +0900

    Create exer1619_pinball.c

---
+++ Chapter16/exer1619_pinball.c
@@ -0,0 +1,13 @@
+// Declare a structure with the following members whose tag is pinball_machine:
+// name - a string of up to 40 characters
+// year - an integer (representing the year of manufacture)
+// type - an enumeration with the values EM (electromechanical) and SS (solid state)
+// players - an integer (representing the maximum number of players)
+
+struct pinball_machine
+{
+	char name[40+1];
+	int year;
+	enum type {EM, SS};
+	int players;
+};
