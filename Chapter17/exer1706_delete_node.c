51909755dad72ff5333828ebe45541f9db85ba5d
FernesRainan <fernes.rainan@gmail.com>
2016-01-14 00:10:11 +0900

    Update exer1706_delete_node.c

--- Chapter17/exer1706_delete_node.c
+++ Chapter17/exer1706_delete_node.c
@@ -1,6 +1,11 @@
 // Modify the delete_from_list function so that it uses only one pointer variable instead
 // of two (cur and prev)
+struct node {
+	int value;
+	struct node *next;
+};
 
+//Original example
 struct node *delete_from_list(struct node *list, int n)
 {
 	struct node *cur, *prev;
@@ -15,4 +20,41 @@ struct node *delete_from_list(struct node *list, int n)
 		prev->next = cur->next; // n is in some other node
 	free(cur);
 	return list;
+}
+
+// First example
+struct node *delete_from_list(struct node *list, int n)
+{
+	struct node *cur;
+	
+	if (list == NULL) return NULL;
+	if (list->value == n) {
+		cur = list->next;
+		free(list);
+		return cur;
+	}
+
+	list->next = delete_from_list(list->next, n);
+	return list;
+}
+
+
+//Second example
+struct node *delete_from_list(struct node *list, int n)
+{
+	struct node **cur;
+	
+	for (cur = &list; *cur != NULL && (*cur)->value != n; cur = &(*cur)->next)
+	;
+	
+	if (*cur != NULL) *cur = my_free(*cur);
+	
+	return list;
+}
+
+struct node *my_free(struct node *node)
+{
+	struct node *next = node->next;
+	free(node);
+	return next;
 }
