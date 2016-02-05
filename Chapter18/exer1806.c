0e4b0f9f73e83ad46d1ac16f1d7adecb5954b07c
FernesRainan <fernes.rainan@gmail.com>
2016-02-05 22:01:13 +0900

    Create exer1806.c

---
+++ Chapter18/exer1806.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// The following function is supposed to print an error message. Each message is preceded by
+// an integer, indicating the number of times the function has been called. Unfortunately, the
+// function always displays 1 as the number of the error message. Locate the error and show
+// how to fix it without making any changes outside the function.
+
+void print_error(const char *message)
+{
+	int n = 1;
+	printf("Error %d: %s\n", n++, message);
+}
+
+void print_error2(const char *message)
+{
+	static int m = 1;
+	printf("Error %d: %s\n", m++, message);
+}
+
+int main(void)
+{
+	int i;
+	
+	char error[] = "This is original error function\n";
+	char error2[] = "This is fixed function\n";
+	
+	for (i = 0; i < 5; i++){
+		print_error(error);
+		print_error2(error2);
+	}
+	
+	return 0;
+}
+
+
+	 
