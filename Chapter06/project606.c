7899035c11cde39f7d49c44145a5d77b6f2321b9
FernesRainan <fernes.rainan@gmail.com>
2016-02-16 22:11:17 +0900

    Create project606.c

---
+++ Chapter06/project606.c
@@ -0,0 +1,20 @@
+// Printing all even squares between 1 and n //
+
+#include <stdio.h>
+
+int main (void)
+{
+    int n, i;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (i = 2; i*i <= n ; i += 2){
+        printf("%d\n", i * i);
+    }
+
+    return 0;
+}
+
+
+
