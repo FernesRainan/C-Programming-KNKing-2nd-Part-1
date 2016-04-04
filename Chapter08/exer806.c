cb4756ad41f831c0a81d9925374e0d631077709e
FernesRainan <fernes.rainan@gmail.com>
2016-04-04 23:20:50 +0900

    Create exer806.c

---
+++ Chapter08/exer806.c
@@ -0,0 +1,12 @@
+//Suppose that we want to set up an array that remembers which segments should be "on" for each digit//
+
+const int segments[10][7] = {{1,1,1,1,1,1,0}, 
+                             {0,1,1,0,0,0,0},
+                             {1,1,0,1,1,0,1},
+                             {1,1,1,1,0,0,1},
+                             {0,1,1,0,0,1,1},
+                             {1,0,1,1,0,1,1},
+                             {1,0,1,1,1,1,1},
+                             {1,1,1,0,0,0,0},
+                             {1,1,1,1,1,1,1},
+                             {1,1,1,1,0,1,1}};
